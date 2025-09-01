774bb75a49001b203a59e8308fd6b58f2677503b
Haroldo Macêdo <haroldo.demacedo@nationwide.co.uk>
2025-09-01 11:37:15 +0100

    Added a Test the AvgStdDev

---
+++ Node/src/test/java/haroldo/stub/metrics/AvgStdDevTest.java
@@ -0,0 +1,33 @@
+package haroldo.stub.metrics;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class AvgStdDevTest {
+    @Test
+    public void stdDevTest()
+    {
+        Random rand = new Random(0);
+        AvgStdDev avgStdDev = new AvgStdDev();
+
+        final int numberOfValues = 100000;
+        long[] values = new long[numberOfValues];
+        for (int i = 0; i < numberOfValues; i++) {
+            values[i] = Math.round(rand.nextGaussian(2000, 200));
+        }
+
+        long ns = System.nanoTime();
+        for (int i = 0; i < numberOfValues; i++) {
+            avgStdDev.addTimeMs(values[i]);
+        }
+        ns = System.nanoTime() - ns;
+
+        System.out.printf("AvgStdDev: %.3fms for %d values or around %3.0fns per value\n",
+                ns/1000000.0, numberOfValues, ((float)ns) / numberOfValues);
+        System.out.println(avgStdDev);
+
+        assert(Math.abs(avgStdDev.getAvg() - 2000) <= 2);
+        assert(Math.abs(avgStdDev.getStdDev() - 200) <= 4);
+    }
+}
